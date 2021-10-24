6ab2de4bb8137b621d7c51892cddd10c904970e1
BorisovMaxim <soulmaxim@gmail.com>
2021-10-24 21:49:04 +0400

    db created by mybatis

---
+++ src/main/java/ru/slmax/utils/DbUtils.java
@@ -0,0 +1,54 @@
+package ru.slmax.utils;
+
+import com.github.javafaker.Faker;
+import lombok.SneakyThrows;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import ru.slmax.db.dao.CategoriesMapper;
+import ru.slmax.db.dao.ProductsMapper;
+import ru.slmax.db.model.Categories;
+import ru.slmax.db.model.CategoriesExample;
+import ru.slmax.db.model.ProductsExample;
+
+import java.io.IOException;
+
+public class DbUtils {
+
+    public static String resource = "mybatisConfig.xml";
+
+    static Faker faker = new Faker();
+
+    private static SqlSession getSqlSession() throws IOException {
+        SqlSessionFactory sqlSessionFactory;
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream(resource));
+        return sqlSessionFactory.openSession(true);
+    }
+
+    @SneakyThrows
+    public static CategoriesMapper getCategoriesMapper() {
+        return getSqlSession().getMapper(CategoriesMapper.class);
+    }
+
+    @SneakyThrows
+    public static ProductsMapper getProductMapper() {
+        return getSqlSession().getMapper(ProductsMapper.class);
+    }
+
+    private static void createNewCategory(CategoriesMapper categoriesMapper) {
+        Categories newCategory = new Categories();
+        newCategory.setTitle(faker.animal().name());
+        categoriesMapper.insert(newCategory);
+    }
+
+    public static Integer countCategories(CategoriesMapper categoriesMapper) {
+        long categoriesCount = categoriesMapper.countByExample(new CategoriesExample());
+        return Math.toIntExact(categoriesCount);
+    }
+
+    public static Integer countProducts(ProductsMapper productsMapper) {
+        long products = productsMapper.countByExample(new ProductsExample());
+        return Math.toIntExact(products);
+    }
+}
